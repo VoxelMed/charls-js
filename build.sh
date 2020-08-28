a7f0d38e0ad5de90b13505948e2fccde5d81fc7e
Richard Malbec <richard@voxelmed.ai>
2020-08-27 23:23:32 -0400

    fix: configuration command uses emcmake (wrapper for emconfigure)

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 mkdir -p build
 #(cd build && emconfigure cmake -DCMAKE_BUILD_TYPE=Debug ..)
-(cd build && emconfigure cmake ..)
+(cd build && emcmake cmake ..)
 (cd build && emmake make VERBOSE=1 -j 16)
 cp ./build/src/charlsjs.js ./dist
 cp ./build/src/charlsjs.wasm ./dist
